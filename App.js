3b0619dc2184cc02083b4caf79f0d1af1d00bfe8
thisissuman <43521912+thisissuman@users.noreply.github.com>
2023-08-09 20:58:59 +0530

    3rd commit

--- App.js
+++ App.js
@@ -1,31 +1,48 @@
 /*
-* HMR (Hot module reolding)
-* File wacther algorithm
-* BUNDLING
-* MINIFY
-* Cleaning our code
-* Dev and production build
-* Super fast build algorithm
-* Image optimization
-* Caching while development
-* Compression
-* Compatitable with older version of browser
-* HTTPS on dev
-* Port number
-* Consistent Hashing algorithm
-* Zero config
-*
-*
-*
-*
-* Transitive dependencies
-*/
+ * HMR (Hot module reolding)
+ * File wacther algorithm
+ * BUNDLING
+ * MINIFY
+ * Cleaning our code
+ * Dev and production build
+ * Super fast build algorithm
+ * Image optimization
+ * Caching while development
+ * Compression
+ * Compatitable with older version of browser
+ * HTTPS on dev
+ * Port number
+ * Consistent Hashing algorithm
+ * Zero config
+ *
+ *
+ *
+ *
+ * Transitive dependencies
+ */
 
-import React from 'react';
-import  ReactDOM  from 'react-dom/client';
-const heading1 = React.createElement("h1", { key: 1 }, "Its form react"); // create Element
-const heading2 = React.createElement("h2", { key: 2 }, "Its form react h2");
-const root = React.createElement("div", {}, [heading1, heading2]); // passing multile child
+import React, { createElement } from "react";
+import ReactDOM from "react-dom/client";
 
-const rootreact = ReactDOM.createRoot(document.getElementById("root")); // create root
-rootreact.render(root); // this render method takes an element and injected in root
+import React from "react";
+import React from 'react'
+
+const Title = () => {
+  return (
+    <div>Title</div>
+  )
+};
+const HeaderComponent = () => {
+  return (
+    <div>
+        <Title/>
+      <h2>Namste React</h2>
+      <h2>This is a h2 page</h2>
+    </div>
+  );
+};
+
+export default Title;
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(<HeaderComponent/>); // this render method takes an element and injected in root
